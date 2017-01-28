54b423655f6a8f9227827ddbaf9a6bbbe9b985f4
Andreas Spading <andylatte@gmx.de>
2017-01-28 15:19:35 +0100

    - edit and delete users from admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -14,7 +14,29 @@ class Admin::UsersController < AdminController
       flash[:error] = "something went wrong."
       render :new
     end
-end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to admin_welcome_index_path
+    else
+      render 'edit'
+    end
+  end  
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to admin_welcome_index_path
+  end
+  
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
