eca30482f1d52eb3830503a2b16de34bdf21eced
Andreas Spading <andylatte@gmx.de>
2017-01-29 18:13:00 +0100

    - routing after sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,17 @@ class ApplicationController < ActionController::Base
   
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+
+  # routing after sign_in
+  def after_sign_in_path_for(resource)
+    if resource.admin?
+      admin_welcome_index_path
+    else
+      welcome_index_path
+    end
+  end
+  
   protected
 
   def configure_permitted_parameters
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,4 @@
 class WelcomeController < ApplicationController
   def index
-    if current_user.admin?
-      redirect_to admin_welcome_index_path
-    end
   end
 end
