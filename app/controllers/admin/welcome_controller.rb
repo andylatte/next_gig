4c33cd79a4081b09ea5e1666aa9fc28627a9d6d5
Andreas Spading <andylatte@gmx.de>
2017-10-29 21:37:45 +0100

    tour sorting admin #33

--- app/controllers/admin/welcome_controller.rb
+++ app/controllers/admin/welcome_controller.rb
@@ -1,5 +1,5 @@
 class Admin::WelcomeController < AdminController
   def index
-    @tours = (current_user.tours_assigned + current_user.tours_managed).uniq
+    @tours = current_user.tours_managed
   end
 end
